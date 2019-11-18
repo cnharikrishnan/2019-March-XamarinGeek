1596477810c957916efa60022b89af24981d07ba
Harikrishnan <cnharishkrish@gmail.com>
2019-11-18 22:08:38 +0530

    Samples and slides committed.

---
+++ Demos/XamarinDemo/XamarinDemo/XamarinDemo/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDemo
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private bool isValidLogin;
+
+        public ViewModel()
+        {
+            this.Username = null;
+            this.Password = null;
+            this.IsValidLogin = false;
+            this.LoginCommand = new Command(ExecuteLoginCommand);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            if(!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                IsValidLogin = true;
+            }
+        }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsValidLogin {
+            get { return this.isValidLogin; }
+            set
+            {
+                this.isValidLogin = value;
+                OnPropertyChanged("IsValidLogin");
+            }
+        }
+
+        public Command LoginCommand { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Xamarin.Forms DependencyService
+================================
+
+Sample code for the [Xamarin.Forms DependencyService](http://developer.xamarin.com/guides/cross-platform/xamarin-forms/dependency-service/) doc.
+
+The sample UsingDependencyService shows how to implement **text to speech** on iOS, Android and the Universal Windows Platform using Xamarin.Forms but calling out to the native platform SDKs for speech.
+
+Xamarin.Forms DataBinding, Custom Renderer
+===========================================
+
+The sample XamarinDemo showcases how to perform DataBinding, different types of DataBinding and writing custom renderer on iOS, Android and the Universal Windows Platform using Xamarin.Forms.
